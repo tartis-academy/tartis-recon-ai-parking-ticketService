d123c81713b8b0c58f608bbcbcad823ef78f207e
Adrianma13 <adrianmachucamorilla@gmail.com>
2026-07-21 10:36:15 +0200

    refactor: Solucion del CI

--- backend/ticket-service/src/main/java/com/tartis_recon_ai_parking/infrastructure/entryticket/adapter/output/persistence/EntryTicketPersistenceAdapter.java
+++ backend/ticket-service/src/main/java/com/tartis_recon_ai_parking/infrastructure/entryticket/adapter/output/persistence/EntryTicketPersistenceAdapter.java
@@ -1,8 +1,5 @@
 package com.tartis_recon_ai_parking.infrastructure.entryticket.adapter.output.persistence;
 
-import com.tartis_recon_ai_parking.application.entryticket.port.output.EntryTicketPersistence;
-
-public class EntryTicketPersistenceAdapter implements EntryTicketPersistence {
-    // TODO @Repository + constructor con EntryTicketRepository y EntryTicketPersistenceMapper
-    // TODO implementar save / findById / findByCode / findByStayId
+// TODO: implements EntryTicketPersistence + @Repository cuando se implementen los metodos
+public class EntryTicketPersistenceAdapter {
 }
