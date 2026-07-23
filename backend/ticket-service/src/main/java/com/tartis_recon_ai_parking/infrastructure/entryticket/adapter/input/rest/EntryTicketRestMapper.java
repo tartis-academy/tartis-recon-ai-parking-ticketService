f26432b78bac405a84fa491dcaf5ae662f2d4f68
Fenrro27 <kjba28sep@gmail.com>
2026-07-24 01:22:58 +0200

    Integracion completa del endpoint con release

--- backend/ticket-service/src/main/java/com/tartis_recon_ai_parking/infrastructure/entryticket/adapter/input/rest/EntryTicketRestMapper.java
+++ backend/ticket-service/src/main/java/com/tartis_recon_ai_parking/infrastructure/entryticket/adapter/input/rest/EntryTicketRestMapper.java
@@ -1,17 +1,19 @@
 package com.tartis_recon_ai_parking.infrastructure.entryticket.adapter.input.rest;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import com.tartis_recon_ai_parking.application.entryticket.dto.EntryTicketCreateDTO;
 import com.tartis_recon_ai_parking.application.entryticket.dto.EntryTicketDTO;
 import com.tartis_recon_ai_parking.infrastructure.entryticket.adapter.input.rest.dto.request.EntryTicketRequest;
 import com.tartis_recon_ai_parking.infrastructure.entryticket.adapter.input.rest.dto.response.EntryTicketResponse;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface EntryTicketRestMapper {
 
     EntryTicketCreateDTO toCreateDTO(EntryTicketRequest request);
 
+    @Mapping(source = "uniqueId", target = "id")
     EntryTicketResponse toResponse(EntryTicketDTO entryTicket);
 
     Iterable<EntryTicketResponse> toResponseList(Iterable<EntryTicketDTO> entryTicket);
