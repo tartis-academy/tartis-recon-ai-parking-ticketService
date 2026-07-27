b7c188c6a965af68101bf018a28c07153ec1d0b7
aleqpneo07 <aleqpneo.07@gmail.com>
2026-07-27 11:21:46 +0200

    feat(ticket): implementa GET ALL de tickets

--- backend/ticket-service/src/main/java/com/tartis_recon_ai_parking/infrastructure/ticket/adapter/output/persistence/TicketPersistenceAdapter.java
+++ backend/ticket-service/src/main/java/com/tartis_recon_ai_parking/infrastructure/ticket/adapter/output/persistence/TicketPersistenceAdapter.java
@@ -3,33 +3,38 @@ package com.tartis_recon_ai_parking.infrastructure.ticket.adapter.output.persist
 import com.tartis_recon_ai_parking.application.ticket.port.output.TicketPersistence;
 import com.tartis_recon_ai_parking.domain.ticket.Ticket;
 import org.springframework.stereotype.Component;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Component
-public class TicketPersistenceAdapter implements TicketPersistence{
+public class TicketPersistenceAdapter implements TicketPersistence {
 
-private final TicketRepository ticketRepository;
-private final TicketPersistenceMapper ticketPersistenceMapper;
+    private final TicketRepository ticketRepository;
+    private final TicketPersistenceMapper ticketPersistenceMapper;
 
-public TicketPersistenceAdapter(TicketRepository ticketRepository, TicketPersistenceMapper ticketPersistenceMapper){
-    this.ticketRepository=ticketRepository;
-    this.ticketPersistenceMapper=ticketPersistenceMapper;
+    public TicketPersistenceAdapter(TicketRepository ticketRepository, TicketPersistenceMapper ticketPersistenceMapper) {
+        this.ticketRepository = ticketRepository;
+        this.ticketPersistenceMapper = ticketPersistenceMapper;
+    }
 
-}
-
-@Override
+    @Override
     public Ticket save(Ticket ticket) {
         TicketEntity entity = ticketPersistenceMapper.toEntity(ticket);
         TicketEntity savedEntity = ticketRepository.save(entity);
         return ticketPersistenceMapper.toDomain(savedEntity);
     }
 
-@Override
+    @Override
     public Optional<Ticket> findById(UUID id) {
         return ticketRepository.findById(id)
                 .map(entity -> ticketPersistenceMapper.toDomain(entity));
     }
 
-    
+    @Override
+    public List<Ticket> findAll() {
+        return ticketRepository.findAll().stream()
+                .map(ticketPersistenceMapper::toDomain)
+                .toList();
+    }
 }
